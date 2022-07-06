dcb0bf7dc62394ce4ad2bae3bff879bcf769c50c
Darren Huang <darrenhuang1211@gmail.com>
2022-07-06 21:53:21 +0800

    Refactored with custom context provider

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../context/auth-context';
 
 function emailReducer(state, action) {
   if (action.type === 'USER_INPUT') {
@@ -38,6 +39,8 @@ const Login = (props) => {
 
   const {isValid: emailIsValid} = emailState;
   const {isValid: passwordIsValid} = passwordState;
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     const identifier = setTimeout(() => {
@@ -79,7 +82,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
